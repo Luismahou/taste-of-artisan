4b00d945d97805af0451dce07607ecbb779f0782
Luisma <luismahou@gmail.com>
2020-01-03 09:25:04 +1100

    feat: fix hero text

--- src/components/sections/hero-section.tsx
+++ src/components/sections/hero-section.tsx
@@ -21,21 +21,23 @@ export const HeroSection = ({
       <div className="relative bg-center bg-cover w-full hero-bg">
         <BgImg imgUrl={imgUrl} />
         <div className="absolute inset-0 flex justify-center items-center sm:justify-start sm:items-end">
-          <div className="relative sm:p-16">
-            <motion.h1
-              className="text-white text-4xl sm:text-6xl text-center sm:text-left uppercase opacity-0"
-              animate={{ opacity: 1, y: -10 }}
-              transition={{ delay: 1, duration: 1 }}
-            >
-              {title}
-            </motion.h1>
-            <motion.h2
-              className="text-white text-2xl sm:text-4xl text-center sm:text-left opacity-0"
-              animate={{ opacity: 1, y: -10 }}
-              transition={{ delay: 1.5, duration: 1 }}
-            >
-              {subtitle}
-            </motion.h2>
+          <div className="container relative">
+            <div className="relative sm:pb-16">
+              <motion.h1
+                className="text-white text-4xl sm:text-6xl text-center sm:text-left uppercase opacity-0"
+                animate={{ opacity: 1, y: -10 }}
+                transition={{ delay: 1, duration: 1 }}
+              >
+                {title}
+              </motion.h1>
+              <motion.h2
+                className="text-white text-2xl sm:text-4xl text-center sm:text-left opacity-0"
+                animate={{ opacity: 1, y: -10 }}
+                transition={{ delay: 1.5, duration: 1 }}
+              >
+                {subtitle}
+              </motion.h2>
+            </div>
           </div>
         </div>
       </div>
