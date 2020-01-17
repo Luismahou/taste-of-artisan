54ec00bd2512697578fd8a0101482195203c59eb
Luisma <luismahou@gmail.com>
2020-01-18 09:21:09 +1100

    fix: 'TitleData' rename

--- src/components/sections/title-section.tsx
+++ src/components/sections/title-section.tsx
@@ -5,13 +5,13 @@ type TitleSectionProps = {
   subtitle: string;
 };
 
-export type TitleData = {
+export type TitleSectionData = {
   kind: 'title';
 } & TitleSectionProps;
 
 export const TitleSection = ({ title, subtitle }: TitleSectionProps) => {
   return (
-    <section className="container text-center py-12 title">
+    <section className="container py-12 text-center title">
       <h1 className="text-4xl sm:text-6xl">{title}</h1>
       <h2 className="text-2xl sm:text-4xl">{subtitle}</h2>
     </section>
