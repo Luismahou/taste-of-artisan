8c24758eedd5e862a3c6bdfd1d137d8684d05549
Luisma <luismahou@gmail.com>
2020-01-18 11:15:37 +1100

    feat: preconnect to google fonts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,6 +27,11 @@ export const Index = ({
     <Head>
       <title>{title}</title>
       <link rel="icon" href="/favicon.ico" />
+      {/*
+       * Preconnects to where font assets really are. It saves some precious
+       * time with low speed connections.
+       */}
+      <link href="https://fonts.gstatic.com" rel="preconnect" crossOrigin="" />
       <link
         href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans&display=swap"
         rel="stylesheet"
