01041e682596e4ddd9e8cc861421daebf3491cb6
Luisma <luismahou@gmail.com>
2020-01-20 10:25:49 +1100

    feat: add separator to TitleSection

--- src/components/sections/title-section.tsx
+++ src/components/sections/title-section.tsx
@@ -14,6 +14,10 @@ export const TitleSection = ({ title, subtitle }: TitleSectionProps) => {
     <section className="container py-12 text-center title">
       <h2 className="text-2xl sm:text-4xl">{title}</h2>
       <h3 className="text-xl sm:text-2xl">{subtitle}</h3>
+      <div
+        style={{ width: 100, height: 4 }}
+        className="mx-auto mt-4 rounded bg-muddy-waters"
+      />
     </section>
   );
 };
