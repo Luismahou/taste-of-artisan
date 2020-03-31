db873e28db1e590140cc575c89e7c32578970c70
Luisma <luismahou@gmail.com>
2020-03-31 23:50:28 +1100

    build: use nextjs default way of dealing with global css

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
-const withCSS = require('@zeit/next-css');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-
-module.exports = withCSS({
-  webpack(config) {
-    config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}));
-
+module.exports = {
+  webpack: (config) => {
     config.module.rules.push({
       test: /\.md$/,
       use: ['raw-loader'],
     });
-
     return config;
   },
-});
+};
