b2f201ad254254874466023e81a46de56d7e7e37
Luisma <luismahou@gmail.com>
2020-05-11 08:43:36 +1000

    fix: missing imgSrc property

--- src/components/base/stories/bg-img.stories.tsx
+++ src/components/base/stories/bg-img.stories.tsx
@@ -3,5 +3,8 @@ import { storiesOf } from '@storybook/react';
 import { BgImg } from '../bg-img';
 
 storiesOf('BgImg', module).add('interactive', () => (
-  <BgImg imgSrcset="http://joepapandrea.com.au/wp-content/uploads/cache/2016/03/joepapandrea_fitout/3232271715.jpg 100w" />
+  <BgImg
+    imgSrc="http://joepapandrea.com.au/wp-content/uploads/cache/2016/03/joepapandrea_fitout/3232271715.jpg"
+    imgSrcset="http://joepapandrea.com.au/wp-content/uploads/cache/2016/03/joepapandrea_fitout/3232271715.jpg 100w"
+  />
 ));
